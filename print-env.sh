cbf60f80563c8e82eadddf15c23e19ca92e2046d
Absa <luisopazo@gmail.com>
2022-04-03 06:47:26 -0400

    Update print-env.sh

--- print-env.sh
+++ print-env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 env | while IFS= read -r line; do
+# ${parameter#pattern} Searches from front to back parameter until the first occurrence of pattern is found and deletes the result.
   value=${line#*=}
+  # ${parameter%%pattern} Searches from back to front of parameter until the last occurrence of pattern is found and deletes the result.
   name=${line%%=*}
   echo "$name:$value" >> env-vars.txt
 done
